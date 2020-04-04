982096cc7be006b7d10f91936c36e773b283dcc4
aleksandrsokolov <timac11@bk.ru>
2020-04-04 23:04:45 +0300

    fix height of map

--- src/YMap.js
+++ src/YMap.js
@@ -3,7 +3,7 @@ import { YMaps, Map } from 'react-yandex-maps';
 
 export const YMap = () => (
     <YMaps>
-        <div style={{display: "flex", width: "100%", height: "300px", justifyContent: "center"}}>
+        <div style={{display: "flex", width: "100%", height: "400px", justifyContent: "center"}}>
             <Map style={{flex: 1, padding: "0 16px"}} defaultState={{ center: [55.75, 37.57], zoom: 4 }} />
         </div>
     </YMaps>
