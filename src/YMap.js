0724d77b27975eefc22aa159a6c3ebae60819047
aleksandrsokolov <timac11@bk.ru>
2020-04-04 23:03:01 +0300

    add stub data to second step

--- src/YMap.js
+++ src/YMap.js
@@ -4,7 +4,7 @@ import { YMaps, Map } from 'react-yandex-maps';
 export const YMap = () => (
     <YMaps>
         <div style={{display: "flex", width: "100%", height: "300px", justifyContent: "center"}}>
-            <Map style={{flex: 0.8}} defaultState={{ center: [55.75, 37.57], zoom: 2 }} />
+            <Map style={{flex: 1, padding: "0 16px"}} defaultState={{ center: [55.75, 37.57], zoom: 4 }} />
         </div>
     </YMaps>
 );
--- src/panels/Result.js
+++ src/panels/Result.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {platform, IOS, Div} from '@vkontakte/vkui';
+import {platform, IOS} from '@vkontakte/vkui';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import PanelHeaderButton from '@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton';
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
+import Icon24User from '@vkontakte/icons/dist/24/user';
 
 import './Persik.css';
 import {YMap} from "../YMap";
+import Avatar from "@vkontakte/vkui/dist/components/Avatar/Avatar";
+import Group from "@vkontakte/vkui/dist/components/Group/Group";
+import HorizontalScroll from "@vkontakte/vkui/dist/components/HorizontalScroll/HorizontalScroll";
+import Header from "@vkontakte/vkui/dist/components/Header/Header";
 
 const osName = platform();
+
+const itemStyle = {
+	flexShrink: 0,
+	width: 80,
+	height: 94,
+	display: 'flex',
+	flexDirection: 'column',
+	alignItems: 'center',
+	fontSize: 12
+};
 
 const Result = props => (
 	<Panel id={props.id}>
@@ -19,9 +34,50 @@ const Result = props => (
 				{osName === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
 			</PanelHeaderButton>}
 		>
-			Result
+			Совпадение
 		</PanelHeader>
-		<YMap/>
+		<Group style={{ paddingBottom: 8 }} header={<Header mode="secondary">Совпадение</Header>}>
+			<HorizontalScroll>
+				<div style={{ display: 'flex' }}>
+					<div style={{ ...itemStyle, paddingLeft: 4 }}>
+						<Avatar size={64} style={{ marginBottom: 8 }}><Icon24User /></Avatar>
+						Элджей
+					</div>
+					<div style={itemStyle}>
+						<Avatar size={64} style={{ marginBottom: 8 }}><Icon24User /></Avatar>
+						Ольга
+					</div>
+					<div style={itemStyle}>
+						<Avatar size={64} style={{ marginBottom: 8 }}><Icon24User /></Avatar>
+						Сергей
+					</div>
+					<div style={itemStyle}>
+						<Avatar size={64} style={{ marginBottom: 8 }}><Icon24User /></Avatar>
+						Илья
+					</div>
+					<div style={itemStyle}>
+						<Avatar size={64} style={{ marginBottom: 8 }}><Icon24User /></Avatar>
+						Алексей
+					</div>
+					<div style={itemStyle}>
+						<Avatar size={64} style={{ marginBottom: 8 }}><Icon24User /></Avatar>
+						Костя
+					</div>
+					<div style={itemStyle}>
+						<Avatar size={64} style={{ marginBottom: 8 }}><Icon24User /></Avatar>
+						Миша
+					</div>
+					<div style={{ ...itemStyle, paddingRight: 4 }}>
+						<Avatar size={64} style={{ marginBottom: 8 }}><Icon24User /></Avatar>
+						Вадим
+					</div>
+				</div>
+			</HorizontalScroll>
+		</Group>
+
+		<Group style={{ paddingBottom: 8 }} header={<Header mode="secondary">Карта передвижения</Header>}>
+			<YMap/>
+		</Group>
 	</Panel>
 );
 
