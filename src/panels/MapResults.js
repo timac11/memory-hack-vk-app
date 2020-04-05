448c9e15b336bdd19b9c07329bab09d97c733cac
aleksandrsokolov <timac11@bk.ru>
2020-04-05 12:45:28 +0300

    add post with attachment

--- src/panels/MapResults.js
+++ src/panels/MapResults.js
@@ -8,6 +8,7 @@ import {IOS, Panel, platform} from "@vkontakte/vkui";
 import PropTypes from "prop-types";
 import PanelHeaderButton from "@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton";
 import PanelHeader from "@vkontakte/vkui/dist/components/PanelHeader/PanelHeader";
+import connect from '@vkontakte/vk-connect';
 
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
@@ -15,6 +16,22 @@ import Icon24Back from '@vkontakte/icons/dist/24/back';
 const osName = platform();
 
 export const MapResults = props => {
+    const vkPost = () => {
+        const info = {};
+        const {fetchedUser} = props;
+
+        info.userid = fetchedUser.id;
+        info.usercity = fetchedUser.city.title;
+        info.userphoto = fetchedUser.photo_200;
+        info.username = fetchedUser.first_name;
+        info.usersurname = fetchedUser.last_name;
+
+        connect.send("VKWebAppShowWallPostBox", {"message": "С 9 мая!", attachment: getAttachment()});
+
+    };
+
+    const getAttachment = () => "photo34917303_457242737";
+
     return <Panel id={props.id}>
         <PanelHeader
             left={<PanelHeaderButton onClick={props.go} data-to="home">
@@ -28,7 +45,7 @@ export const MapResults = props => {
         </Group>
 
         <Div>
-            <Button size="xl">Опубликовать на стене</Button>
+            <Button onClick={() => vkPost()} size="xl">Опубликовать на стене</Button>
         </Div>
     </Panel>
 };
